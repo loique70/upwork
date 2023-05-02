3a68ec247dd91e1532796904d3c772031fb98e26
loique70 <fogouangloic10@gmail.com>
2023-05-02 02:55:22 -0700

    loique

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          Hello Loique wlcome to your firt deployed app
+          Hello Loique welcome to your firt deployed app
           <h1>What is up there.....????.</h1>
           <p>We are lionnes</p>
       </div>
