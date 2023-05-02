e6fdf71f5b7dedfeefda4a2ffaee3e6223690c23
loique70 <fogouangloic10@gmail.com>
2023-05-02 03:08:55 -0700

    yoche

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           Hello Loique welcome to your firt deployed app
-          <h1>What is up there.....????.</h1>
+          <h1>What is up there</h1>
           <p>We are lioness</p>
       </div>
     </main>
